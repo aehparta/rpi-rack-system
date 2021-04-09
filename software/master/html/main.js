4c80d9b3865dc422cc73c4fcd6109334b3bfbe3c
Antti Partanen <aehparta@iki.fi>
2021-04-09 20:35:57 +0100

    ui is better

--- software/master/html/main.js
+++ software/master/html/main.js
@@ -1,21 +1,82 @@
-/* connect socket.io to server */
-const host = "rack";
-const port = 8000;
-const socket = io.connect("http://" + host + ":" + port);
-const term = new Terminal({
-  rows: 40,
-});
+(function ($) {
+  $.each(["show", "hide"], function (i, ev) {
+    var el = $.fn[ev];
+    $.fn[ev] = function () {
+      this.trigger(ev);
+      return el.apply(this, arguments);
+    };
+  });
+})(jQuery);
 
-term.open(document.getElementById("terminal"));
+$(document).ready(() => {
+  let currentSlotId = undefined;
 
-term.onKey((event) => {
-  socket.emit("terminal", event.key);
-});
+  $("body").on("click", "nav>button", (e) => {
+    $("nav>button").removeClass("selected");
+    $(e.target).addClass("selected");
+  });
 
-socket.on("terminal", (data) => {
-  term.write(data);
-});
+  $("body").on("click", "button.toggle", (e) => {
+    $(e.target).toggleClass("on");
+  });
 
-socket.on("current", (data) => {
-  document.getElementById("current-value").innerHTML = Number(data).toFixed(3);
+  $("body").on("click", "button[view]", (e) => {
+    const view = $(e.target).attr("view");
+    $(view).parent().children().hide();
+    $(view).show();
+    currentSlotId = undefined;
+    if (view.startsWith("#slot-")) {
+      currentSlotId = Number($(view).attr("slot"));
+      term.reset();
+      $("#terminal").show();
+      term.resize(80, 30);
+      term.write(slots[currentSlotId].lastlog);
+    }
+  });
+
+  /* slots array */
+  const slots = [];
+
+  /* initialize terminal */
+  const term = new Terminal({
+    cols: 40,
+    rows: 30,
+    cursorBlink: "block",
+  });
+  term.open(document.getElementById("terminal"));
+
+  term.onKey((event) => {
+    socket.emit("terminal", "0", event.key);
+  });
+
+  /* connect socket.io to server */
+  const socket = io.connect("http://rack:8000");
+
+  socket.on("slots", (data) => {
+    const template = $("#template-slot").html();
+    for (let slotId = 0; slotId < data.length; slotId++) {
+      slots.push(data[slotId]);
+      $("main").prepend($(template).attr("id", `slot-${slotId}`));
+      $(`#slot-${slotId} .label`).val(data[slotId].label);
+      $(`#slot-${slotId}`).attr("slot", slotId);
+      if (slotId === 0) {
+        $(`#slot-${slotId} .master-hidden`).hide();
+      }
+    }
+  });
+  socket.on("terminal", (slotId, data) => {
+    slots[slotId].lastlog += data;
+    if (slotId === currentSlotId) {
+      term.write(data);
+    }
+  });
+  socket.on("U", (slotId, value) => {
+    $(`#slot-${slotId} .measurement .U`).text((value || 0).toFixed(1));
+  });
+  socket.on("I", (slotId, value) => {
+    $(`#slot-${slotId} .measurement .I`).text((value || 0).toFixed(2));
+  });
+  socket.on("P", (slotId, value) => {
+    $(`#slot-${slotId} .measurement .P`).text((value || 0).toFixed(0));
+  });
 });
--- software/master/main.js
+++ software/master/main.js
@@ -1,63 +1,70 @@
-const SPI = require("spi-device");
+const spiDevice = require("spi-device");
 const gpio = require("onoff").Gpio;
-
-/*
- * Ip address and port where to listen for incoming connections.
- * Set ip as "0.0.0.0" to listen all.
- */
-const host = "0.0.0.0";
-const port = 8000;
-
-/*
- * Initialize node and socket.io plus url parser and filesystem reader.
- */
-const http = require("http").createServer(httpHandler);
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "http://rack",
-    methods: ["GET", "POST"],
-  },
-});
+const yaml = require("yaml");
 const fs = require("fs");
 
-const terminalInputQueue = [];
-const lastlog = [""];
+let config = {
+  url: "http://rack:8000",
+  bind: "0.0.0.0",
+  port: 8000,
+  slots: [
+    { label: "Master" },
+    { label: "Slot #1" },
+    { label: "Slot #2" },
+    { label: "Slot #3" },
+    { label: "Slot #4" },
+    { label: "Slot #5" },
+    { label: "Slot #6" },
+    { label: "Slot #7" },
+    { label: "Slot #8" },
+    { label: "Slot #9" },
+    { label: "Slot #10" },
+    { label: "Slot #11" },
+  ],
+};
 
-/* start listening incoming connections */
-http.listen(port, host);
-/* setup socket.io new connection events handler */
-io.sockets.on("connection", (socket) => {
-  socket.on("terminal", (data) => {
-    /* we are assuming here that data is send in less than 8 characters at a time */
-    terminalInputQueue.push(Buffer.from(data.padEnd(8, "\0")));
-  });
-  socket.emit("terminal", lastlog.join(""));
-});
+try {
+  /* read config from file */
+  const content = yaml.parse(fs.readFileSync("config.yml", "utf-8"));
+  if (content) {
+    config = { ...config, content };
+  }
+} catch (e) {}
+
+process.stdout.write(
+  `url: ${config.url}\nbind: ${config.bind}:${config.port}\nslots: ${config.slots.length}\n`
+);
+
+const slots = [];
+for (let slotId = 0; slotId < config.slots.length; slotId++) {
+  slots[slotId] = {
+    inputQueue: [],
+    lastlog: [""],
+    I: 0,
+    U: 0,
+    P: 0,
+    count: 0,
+  };
+}
 
 /*
- * Handle http requests here. Socket.io request are automatically served
- * before this.
+ * HTTP
  */
-function httpHandler(request, response) {
-  /* If this is a simple GET request into filesystem */
+
+const httpHandler = (request, response) => {
   if (request.method === "GET") {
     httpFileServe(request, response, request.url);
   } else {
     httpResponse404(response);
   }
-  /* Not handling any other requests for now */
-}
+};
 
-/*
- * Server file from filesystem.
- */
-function httpFileServe(request, response, filename) {
+const httpFileServe = (request, response, filename) => {
   if (filename === "/") {
     filename = "/index.html";
   }
   console.log("serve file " + filename);
 
-  /* Try sending the file, or 404 in errors */
   fs.readFile(__dirname + "/html" + filename, function (err, data) {
     if (err) {
       return httpResponse404(response);
@@ -66,16 +73,45 @@ function httpFileServe(request, response, filename) {
     response.writeHead(200);
     response.end(data);
   });
-}
+};
 
-/*
- * Wrapper for sending 404.
- */
-function httpResponse404(response) {
+const httpResponse404 = (response) => {
   response.writeHead(404);
   return response.end("File not found");
-}
+};
 
+const http = require("http").createServer(httpHandler);
+http.listen(config.port, config.bind);
+
+/*
+ * socket.io
+ */
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: config.url,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.sockets.on("connection", (socket) => {
+  socket.on("terminal", (slotString, data) => {
+    const slot = Number(slotString);
+    if (slot >= 0 || slot < slots.length) {
+      /* we are assuming here that data is send in less than 8 characters at a time */
+      slots[slot].inputQueue.push(Buffer.from(data.padEnd(8, "\0")));
+    }
+  });
+  socket.emit(
+    "slots",
+    slots.map((slot, index) => ({
+      ...config.slots[index],
+      lastlog: slot.lastlog.join(""),
+    }))
+  );
+});
+
+/* GPIO */
 const cs = [
   new gpio(18, "out"),
   new gpio(23, "out"),
@@ -83,17 +119,26 @@ const cs = [
   new gpio(25, "out"),
 ];
 const rst = new gpio(7, "out");
-
 cs.forEach((pin) => pin.writeSync(0));
 rst.writeSync(1);
+const slotSelect = (slotId) => {
+  cs[0].writeSync(slotId & 0x01 ? 1 : 0);
+  cs[1].writeSync(slotId & 0x02 ? 1 : 0);
+  cs[2].writeSync(slotId & 0x04 ? 1 : 0);
+  cs[3].writeSync(slotId & 0x08 ? 1 : 0);
+};
 
-const spi = SPI.openSync(0, 0);
+/* SPI */
+const spi = spiDevice.openSync(0, 0);
 
-const calc = { v: 0, c: 0 };
+const readSpi = (slotId) => {
+  slotSelect(slotId);
 
-const readSpi = () => {
   const sendBuffer = Buffer.concat(
-    [Buffer.from([0x01, 0x02]), terminalInputQueue.pop() || Buffer.from([])],
+    [
+      Buffer.from([0x01, 0x02]),
+      slots[slotId].inputQueue.pop() || Buffer.from([]),
+    ],
     10
   );
   const data = [
@@ -101,20 +146,23 @@ const readSpi = () => {
       sendBuffer,
       receiveBuffer: Buffer.alloc(10),
       byteLength: 10,
-      speedHz: 5e5,
+      speedHz: 8e5,
     },
   ];
   spi.transfer(data, () => {
-    calc.v += (data[0].receiveBuffer[2] - 2) * 8.2;
-    calc.c++;
+    slots[slotId].U += 15.6;
+    slots[slotId].I +=
+      data[0].receiveBuffer[2] > 0 ? (data[0].receiveBuffer[2] - 2) * 8.2 : 0;
+    slots[slotId].count++;
 
     const noInvalids = data[0].receiveBuffer.filter(
       (ch, index) => ch > 7 && index != 1 && index != 2
     );
     if (noInvalids.length > 0) {
       const str = Buffer.from(noInvalids).toString();
-      io.emit("terminal", str);
+      io.emit("terminal", slotId, str);
       /* append to log */
+      const lastlog = slots[slotId].lastlog;
       str.split(/(?=\n)/g).forEach((part) => {
         if (lastlog[lastlog.length - 1].slice(-1) !== "\n") {
           lastlog[lastlog.length - 1] += part;
@@ -123,18 +171,28 @@ const readSpi = () => {
         }
       });
       /* shorten log to maximum lines */
-      while (lastlog.length > 10) {
+      while (lastlog.length > 1000) {
         lastlog.shift();
       }
     }
-    readSpi();
+    slotId++;
+    readSpi(slotId < slots.length ? slotId : 0);
   });
 };
 
-readSpi();
+readSpi(0);
 
 setInterval(() => {
-  io.emit("current", calc.v / calc.c / 1000);
-  calc.v = 0;
-  calc.c = 0;
+  slots.forEach((slot, slotId) => {
+    const U = slot.U / slot.count;
+    const I = slot.I / slot.count / 1000;
+    const P = U * I;
+    io.emit("U", slotId, U);
+    io.emit("I", slotId, I);
+    io.emit("P", slotId, P);
+    slot.U = 0;
+    slot.I = 0;
+    slot.P = P;
+    slot.count = 0;
+  });
 }, 500);
